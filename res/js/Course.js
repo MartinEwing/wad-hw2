4c7c6d1cff6d55e7595ff4874475d134b0c086a4
MartinEwing <mart99in.server@gmail.com>
2019-10-13 23:07:37 +0300

    Made "+" button clickable

--- res/js/Course.js
+++ res/js/Course.js
@@ -7,3 +7,13 @@ class Course {
     }
 
 }
+
+var hidden = true;
+function addCourse() {
+    hidden = !hidden;
+    if(hidden) {
+        document.getElementById('add-course').style.display = 'inline';
+    } else {
+        document.getElementById('add-course').style.display = 'none';
+    }
+}
